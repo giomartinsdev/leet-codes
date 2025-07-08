7d715cd3428c839df6def3aa48fe0275e8fb7ebb
giomartinsdev <giovannidealmeidamartins@gmail.com>
2025-07-07 22:11:13 -0300

    feat: add 121-Best-Time-to-Buy-and-Sell-Stock project and implementation

---
+++ src/121-Best-Time-to-Buy-and-Sell-Stock/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int position = int.MaxValue;
+        int maxProfit = 0;
+        
+        foreach (int price in prices)
+        {
+            int net = price - position;
+            if (net > maxProfit) maxProfit = net;
+
+            if (price < position) position = price;
+        }
+        return maxProfit;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var solution = new Solution();
+        int[] nums = [2, 4, 1];
+        int result = solution.MaxProfit(nums);
+        Console.WriteLine(result);
+    }
+}
